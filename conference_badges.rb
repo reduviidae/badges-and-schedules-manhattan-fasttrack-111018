bfc4ffef4d8c1870babd063f21831a70a69dc7ba
Wil Cook <wil.cook.is@gmail.com>
2018-11-11 20:33:49 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,16 +8,22 @@ def badge_maker(name)
   puts "Hello, my name is #{name}."
 end
 
+#for each name in an array, execute the badge_maker method one time
 def batch_badge_creator(array)
   array.each do |name|
     badge_maker(name)
   end
 end
 
+#there are seven rooms numbered 1-7 and seven speakers indexed 0-6, this method prints a message to assign a room to a speaker by index number plus one to make the numbers match.
 def assign_rooms(speakers)
   speakers.each_with_index { |item, index| p "#{item} is assigned to room #{index + 1}." }
 end
 
 
-batch_badge_creator(attendees)
-assign_rooms(attendees)
+def printer(array)
+  batch_badge_creator(array)
+  assign_rooms(array)
+end
+
+printer(attendees)
